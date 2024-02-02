f7977ef1e23e28a8a6d5d8ae8ff9b0be3c423787
vasudev.parmar <vsdvprmr@gmail.com>
2024-02-02 14:57:26 +0530

    different kind of function declaration and their execution

--- Javascript/code-execution-example.js
+++ Javascript/code-execution-example.js
@@ -7,17 +7,31 @@
  *  console.log("Hello world");
  *  }
  * 
- *  because in first phase of code execution Memory is already allocated for x= undefined and for fn it will copy whole fn
- *  when code get executed we get undefined for the x and function get executed because in memory it's available
+ *  because in first phase of code execution Memory is already allocated for x= undefined and for fn it will copy 
+ *  whole fn when code get executed we get undefined for the x and function get executed because in memory it's available
  * 
  *  Note:- If we have not defined x anywhere then in memory it is not allocated so while running the code 
  *  output will print error (ReferenceError: x is not defined)  
  */
 
 console.log(x);
+getName2(); //error getName2 is not a function, as it will be treated as the variable
 getName();
 console.log(getName);
 
 function getName(){
     console.log("Hello world");
+}
+
+/**
+ * For getName2 and getName3 in first phase memory allocation will happen and will be treated as the variable 
+ * and allocated as undefined
+ */
+
+var getName2= function (){
+    console.log("getName2");
+}
+
+var getName3= () =>{
+    console.log("getName3");
 }
